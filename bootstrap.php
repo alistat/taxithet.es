095efa07b108b21a6ec4c33a21f478672cd462e4
alistat <alieus@hotmail.gr>
2023-05-24 19:02:35 +0300

    FIX: Locate .env in root of parent directory

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1));
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
 $dotenv->safeLoad();
 
 ?>
