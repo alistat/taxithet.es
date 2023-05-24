c95ee7626b18ec029f681da00683718760806e9a
alistat <alieus@hotmail.gr>
2023-05-24 04:02:56 +0300

    Test .env module

--- api/reservation.php
+++ api/reservation.php
@@ -1,4 +1,9 @@
 <?php
-echo getenv('testvar');
-
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+echo "in \n";
+echo $_ENV['TESTAVAR'];
+echo "\n 2 \n";
+echo $_ENV['TEST_VAR'];
+echo "\n no cras \n";
 ?>
